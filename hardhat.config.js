c8084dc929b52a044b82f3e9e2a52d71e5f78cf8
Berkan Izgi <biz3926@students.fhv.at>
2025-12-03 20:43:31 +0100

    bug fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
   networks: {
@@ -11,5 +9,7 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
-

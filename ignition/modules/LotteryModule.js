36f6a0c268eafbcc76fe1c4d191c9b7dee1f3870
Berkan Izgi <biz3926@students.fhv.at>
2025-12-04 16:34:17 +0100

    clean up

--- ignition/modules/LotteryModule.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("LotteryModule", (m) => {
-    const ticketPrice = m.getParameter("ticketPrice", 10n ** 16n);
-
-    const lottery = m.contract("Lottery", [ticketPrice]);
-
-    return { lottery };
-});
